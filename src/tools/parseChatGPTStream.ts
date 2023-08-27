836baf64f21874bb0fec4f5875199ba3f355543f
Joe Taylor <accounts+github@joetaylor.net>
2023-08-28 00:15:26 +0100

    got streaming from openai working and integrated in frontend

--- src/tools/parseChatGPTStream.ts
+++ src/tools/parseChatGPTStream.ts
@@ -8,7 +8,7 @@ export async function ChatGPTStream(payload:any) {
   const res = await fetch("https://api.openai.com/v1/chat/completions", {
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer ${process.env.OPENAI_API_KEY ?? ""}`,
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY ?? "FOO"}`,
     },
     method: "POST",
     body: JSON.stringify(payload),
