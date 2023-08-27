9e98f9a83bd6118569d16558cdb782c3ea315cfd
Joe Taylor <accounts+github@joetaylor.net>
2023-08-27 23:58:52 +0100

    got streaming from openai working and integrated in frontend

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -34,9 +34,11 @@ export function Message({keyProp, role, content, timestamp, typewriter}: Message
         >
         <div className={bubbleOuter}>
             <div className={bubble}>
-              {// eslint-disable-next-line
-              }
-                <div className={bubbleText}><ReactMarkdown children={content ? content : ''} remarkPlugins={[remarkGfm]} /></div>
+                <div className={bubbleText}>
+                  {// eslint-disable-next-line
+                  }
+                  <ReactMarkdown children={content ? content : ''} remarkPlugins={[remarkGfm]} />
+                </div>
             </div>
             <p className='text-xs text-neutral-400/80 dark:text-neutral-500/70 tracking-normal'>
               {
